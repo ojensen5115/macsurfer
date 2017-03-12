18cf3bb9d2f92b378577eb84ee2546a9035ab2e9
Oliver Jensen <ojensen5115@gmail.com>
2017-03-12 15:36:42 -0500

    separate out waiting for connection vs waiting for curl

--- main.py
+++ main.py
@@ -1,30 +1,43 @@
+import netifaces as ni
+
 import random
 import subprocess
 import sys
 import time
 
 
-def waitForAndCheckInternet(wait=20):
-    # for now we just keep trying curl until it doesn't fail somehow
-    # assume we're OK if we don't get a redirect
-    # TODO: refactor to use udev
-    # TODO: sometimes association hangs and this can cause a false negative
-    increment = 0.5
-    while wait > 0:
+def waitForAndCheckInternet():
+    interval = 0.5
+    # first, wait until we've reconnected to the wifi
+    # we give this a timeout of 10 seconds before trying again
+    timeout = 10
+    while ni.AF_INET not in ni.ifaddresses(iface):
+        timeout -= interval
+        if timeout == 0:
+            timeout = 20
+            # restart the interface
+            resetInterface(None)
+        time.sleep(interval)
+
+    # check a known 200 response from CURL
+    # sometimes this can randomly take a while so retry if > 5 seconds
+    # if we've been doing this for > 30 seconds, someting bad happened
+    for loopCounter in range(12):
         try:
-            result = subprocess.run(('curl', '-I', 'neverssl.com'), stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, timeout=6)
+            result = subprocess.run(('curl', '-I', 'neverssl.com'),
+                stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, timeout=5)
             if result.returncode == 0:
                 break
-            wait -= increment
             time.sleep(increment)
         except:
             # curl timed out? try again
             pass
     return result.stdout[:15] == b'HTTP/1.0 200 OK'
 
-def setMac(mac):
+def resetInterface(mac):
     subprocess.run(('service', 'network-manager', 'stop'), stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-    subprocess.run(('macchanger', '-m', mac, iface), stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    if mac:
+        subprocess.run(('macchanger', '-m', mac, iface), stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
     subprocess.run(('service', 'network-manager', 'start'), stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
 def run(candidates, keepGoing=False):
@@ -32,7 +45,7 @@ def run(candidates, keepGoing=False):
     total = len(candidates)
     for idx, mac in enumerate(candidates):
         print("Attempt {}/{} ({})".format(idx+1, total, mac))
-        setMac(mac)
+        resetInterface(mac)
         success = waitForAndCheckInternet()
         if success:
             print("    SUCCESS!")
@@ -45,7 +58,7 @@ def run(candidates, keepGoing=False):
             print(mac)
         selected = valid[int(random.random() * len(valid))]
         print("\nUsing: {}".format(selected))
-        setMac(valid[0])
+        resetInterface(valid[0])
 
 # for now, hardcode these values
 keepGoing = True
